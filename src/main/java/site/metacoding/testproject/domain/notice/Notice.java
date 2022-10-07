a1c9d3c7da44233e1eec5e048c0eae87d36e1753
jinsa <jinsa004@gmail.com>
2022-10-07 15:18:38 +0900

    내 공고 수정하기 Junit 테스트 완료

--- src/main/java/site/metacoding/testproject/domain/notice/Notice.java
+++ src/main/java/site/metacoding/testproject/domain/notice/Notice.java
@@ -6,7 +6,8 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Notice {
-    private Integer noticeId;
+
+	private Integer noticeId;
 	private Integer companyId;
 	private String noticeTitle;
 	private String noticeConame;
@@ -20,4 +21,16 @@ public class Notice {
     private String noticeCareer;
     private String noticeWellfare;
 	private Integer jobId;
+
+	public Notice(String noticeConame, String noticeTitle, String noticeSal) {
+		this.noticeConame = noticeConame;
+		this.noticeTitle = noticeTitle;
+		this.noticeSal = noticeSal;
+	}
+
+	public void update(Notice notice) {
+		this.noticeConame = notice.getNoticeConame();
+        this.noticeTitle = notice.getNoticeTitle();
+        this.noticeSal = notice.getNoticeSal();
+    }
 }
---
+++ src/test/java/site/metacoding/testproject/domain/notice/NoticeDaoTest.java
@@ -0,0 +1,36 @@
+package site.metacoding.testproject.domain.notice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import site.metacoding.testproject.config.MyBatisConfig;
+
+@Import(MyBatisConfig.class) // MyBatisTest가 MyBatisConfig를 못읽음
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 실DB사용
+@MybatisTest
+public class NoticeDaoTest {
+    
+    @Autowired
+    private NoticeDao noticeDao;
+
+    @Test
+    public void updateNotice() {
+        //give
+        Integer noticeId = 1;
+        String noticeConame = "하늘";
+        String noticeTitle = "늘 푸른 사원 모집";
+        String noticeSal = "2400만원";
+
+        Notice notice = new Notice(noticeConame, noticeTitle, noticeSal);
+        //영속 
+        Notice noticePS = noticeDao.findById(noticeId);
+        //when
+        noticePS.update(notice);
+        //then
+        assertEquals("하늘", noticePS.getNoticeConame());
+    }
+}
